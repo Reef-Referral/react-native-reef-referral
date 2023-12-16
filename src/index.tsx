c0b971c05aa5c125300126b0ebd714ef5b0a03a4
ReefReferral <contact@reefreferral.com>
2023-12-16 17:26:15 +0100

    docs: add jsdocs

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ export * from './types';
 
 const Emitter = new NativeEventEmitter(ReefReferral);
 
+/** Start Reef Referral SDK with provided API key */
 export async function startAsync({
   apiKey,
 }: {
@@ -15,22 +16,27 @@ export async function startAsync({
   return await ReefReferral.startAsync(apiKey);
 }
 
+/** Get current referral status */
 export async function getReferralStatusAsync(): Promise<ReferralStatus | null> {
   return await ReefReferral.getReferralStatusAsync();
 }
 
+/** Notify SDK that the app has been opened from a deep link */
 export async function handleDeepLinkAsync(url: string): Promise<void> {
   return await ReefReferral.handleDeepLinkAsync(url);
 }
 
+/** Notify SDK that the sender (referrer) reward has been granted by your app */
 export async function triggerSenderSuccessAsync(): Promise<void> {
   return await ReefReferral.triggerSenderSuccessAsync();
 }
 
+/** Notify SDK that the receiever (referred user) reward has been granted by your app */
 export async function triggerReceiverSuccessAsync(): Promise<void> {
   return await ReefReferral.triggerReceiverSuccessAsync();
 }
 
+/** Set the user ID. By default, an anonymous ID will be generated. */
 export async function setUserIdAsync(userId: string): Promise<void> {
   return await ReefReferral.setUserIdAsync(userId);
 }
@@ -44,6 +50,12 @@ export const addEventListener = (
 
 export const removeAllListeners = Emitter.removeAllListeners;
 
+/**
+ * Always returns current referral status as it changes.
+ * `null` return value means value has not been read yet
+ * or could not been read, like if the library is used
+ * on an unsupported platform.
+ */
 export const useReferralStatus = (): {
   referralStatus: ReferralStatus | null;
 } => {
--- src/types.ts
+++ src/types.ts
@@ -11,14 +11,27 @@ export enum SenderRewardStatus {
 }
 
 export type ReceiverStatus = {
+  /**
+   * The App Store Offer code URL that unlocks the receiver reward (in Standard Mode).
+   * Opening this URL will open the App Store app and show a prompt asking the user
+   * to accept the discounted offer.
+   */
   offerCodeUrl: string | null;
+  /**
+   * Eligiblity of the current user as receiver. The user becomes eligible
+   * after opening the referral link that is then handled by `handleDeepLinkAsync`.
+   */
   rewardEligibility: ReceiverOfferStatus;
 };
 
 export type SenderStatus = {
+  /** Current user's referral URL */
   linkUrl: string | null;
+  /** Sender reward offer code redemption URL */
   offerCodeUrl: string | null;
+  /** Number of redeemed offers from this link */
   redeemedCount: number;
+  /** Eligibility of the sender reward */
   rewardEligibility: SenderRewardStatus;
 };
 
