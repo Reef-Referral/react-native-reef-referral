c6873f7f93051d1642641e1f6b42da5a7891b227
ReefReferral <contact@reefreferral.com>
2023-12-17 20:51:11 +0100

    feat: support more cocoapods projects

--- ios/RNReefReferralModule.h
+++ ios/RNReefReferralModule.h
@@ -1,7 +1,13 @@
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNReefReferral.h"
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#elif __has_include("RCTEventEmitter.h")
 #import "RCTEventEmitter.h"
+#elif __has_include("React/RCTEventEmitter.h")
+#import "React/RCTEventEmitter.h"
+#endif
 
 @interface RNReefReferralModule : RCTEventEmitter <NativeReefReferralSpec>
 #else
